6f30313e6c7fc3d7255c695bf65f6dabf2c99c9c
fyagiz <furkanyagiz@gmail.com>
2020-05-01 02:10:06 +0300

    Create Nurse Class

---
+++ src/nurse.cpp
@@ -0,0 +1,44 @@
+#include <cstring>
+#include <iostream>
+#include "nurse.h"
+
+Nurse::Nurse(){
+    Employee();
+    nTitle = General;
+}
+
+Nurse::Nurse(int employeeID, int telephoneNumber, int annualSalary, const char *firstName, const char *lastName, char const *address, char const *email, nurseTitle title) 
+                : nTitle(title), Employee(employeeID, telephoneNumber, annualSalary, firstName, lastName, address, email) {}
+
+const char *Nurse::getTitle(){
+
+    switch(nTitle){
+        case 0:
+            return "General";
+            break;
+        case 1:
+            return "Midwife";
+            break;
+    }
+    return NULL;
+}
+
+void Nurse::setTitle(nurseTitle title){
+    this->nTitle = title;
+}
+
+int Nurse::overtimeAnnualSalaryCalculate(int overTime){
+    int newAnnualSalary = getAnnualSalary();
+
+    if (!strcmp("General", getTitle())){
+        newAnnualSalary += (overTime * 20) * 52;
+        setAnnualSalary(newAnnualSalary);
+        return newAnnualSalary;
+    }
+
+    else{
+        newAnnualSalary += (overTime * 30) * 52;
+        setAnnualSalary(newAnnualSalary);
+        return newAnnualSalary;
+    }
+}
---
+++ src/nurse.h
@@ -0,0 +1,28 @@
+#ifndef NURSE_H
+#define NURSE_H
+#include "employee.h"
+
+enum nurseTitle {General, Midwife};
+
+class Nurse : public Employee{
+
+    private:
+        nurseTitle nTitle;
+    
+    public:
+
+        // Default constructor
+        Nurse();
+        // Specialized constructor
+        Nurse(int, int, int, const char *, const char *, const char *, const char *, nurseTitle);
+
+        // Getter and Setter Functions
+        const char *getTitle();
+        void setTitle(nurseTitle);
+
+        // Annual Salary Calculator
+        int overtimeAnnualSalaryCalculate(int);
+
+};
+
+#endif
